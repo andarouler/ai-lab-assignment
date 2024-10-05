0c6950969e14f111cd375703cd449b18723f5639
andi <schief19@ads.uni-passau.de>
2024-10-05 13:27:46 +0200

    add ArtStyleDataset

--- datasets.py
+++ datasets.py
@@ -13,8 +13,8 @@ class WeatherDataset(Dataset):
         # Lade die Bilder aus den entsprechenden Verzeichnissen und setze Labels
         for label, subdir in enumerate(['rainy', 'sunny']):  # 0 für rainy, 1 für sunny
             # Pfade für beide rainy und sunny Typen (artistic & realistic)
-            for weather_type in ['artistic', 'realistic']:
-                full_dir = os.path.join(root_dir, f"{weather_type}_{subdir}")
+            for art_style in ['artistic', 'realistic']:
+                full_dir = os.path.join(root_dir, f"{art_style}_{subdir}")
                 for img_file in os.listdir(full_dir):
                     if img_file.endswith(('.png')):  # Füge die passenden Bildformate hinzu
                         self.image_paths.append(os.path.join(full_dir, img_file))
@@ -23,6 +23,39 @@ class WeatherDataset(Dataset):
 
         print(f"Image count: {len(self.image_paths)}")
 
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
+
+class ArtStyleDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+        
+        # Lade die Bilder aus den entsprechenden Verzeichnissen und setze Labels
+        for label, subdir in enumerate(['artistic', 'realistic']):  # 0 für rainy, 1 für sunny
+            # Pfade für beide rainy und sunny Typen (artistic & realistic)
+            for weather_type in ['rainy', 'sunny']:
+                full_dir = os.path.join(root_dir, f"{subdir}_{weather_type}")
+                for img_file in os.listdir(full_dir):
+                    if img_file.endswith(('.png')):  # Füge die passenden Bildformate hinzu
+                        self.image_paths.append(os.path.join(full_dir, img_file))
+                        self.labels.append(label)  # 0 für rainy, 1 für sunny
+                        print(f"Image-file: {img_file}, label: {label}, path: {full_dir}")
+
+        print(f"Image count: {len(self.image_paths)}")
+    
     def __len__(self):
         return len(self.image_paths)
     
